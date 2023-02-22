4ad86ef7bb65860cdce5b657c413181086ea34aa
shabbir rangwala <shabbir.ahr@gmail.com>
2023-02-22 16:18:42 +0530

    server modified

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.delete('/api/courses/:id', (req, res) => {
   }    
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}...`);
 });
